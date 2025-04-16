2fa298359bd58aaa9f6e25b9ca05344cc542c86a
agsjohn <joaovitormds03@gmail.com>
2025-04-16 14:51:20 -0300

    Fim imersão Alura

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/data/restaurant_data.dart';
 import 'package:my_app/ui/_core/app_theme.dart';
+import 'package:my_app/ui/_core/bag_provider.dart';
 import 'package:my_app/ui/splash/splash_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +17,7 @@ void main() async {
             return restaurantData;
           },
         ),
+        ChangeNotifierProvider(create: (context) => BagProvider()),
       ],
       child: MyApp(),
     ),
@@ -27,6 +29,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
+    return MaterialApp(
+      theme: AppTheme.appTheme,
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
---
+++ lib/ui/_core/bag_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/model/dish.dart';
+
+class BagProvider extends ChangeNotifier {
+  List<Dish> dishesOnBag = [];
+
+  void addAllDishes(List<Dish> dishes) {
+    dishesOnBag.addAll(dishes);
+    notifyListeners();
+  }
+
+  void removeDish(Dish dish) {
+    dishesOnBag.remove(dish);
+    notifyListeners();
+  }
+
+  void clearBag() {
+    dishesOnBag.clear();
+    notifyListeners();
+  }
+
+  Map<Dish, int> getMapByAmount() {
+    Map<Dish, int> mapResult = {};
+    for (Dish dish in dishesOnBag) {
+      if (mapResult[dish] == null) {
+        mapResult[dish] = 1;
+      } else {
+        mapResult[dish] = mapResult[dish]! + 1;
+      }
+    }
+
+    return mapResult;
+  }
+}
